547a21dd82071eaf531cdeafb746fce24d8deeed
StefanoMela <stefano.mela25@gmail.com>
2023-07-24 17:04:57 +0200

    feat: try w/ arrays

--- js-dadi/JS/myscript.js
+++ js-dadi/JS/myscript.js
@@ -1,11 +1,10 @@
-console.log("JS OK");
+// METODO RANDOM
 
 // COSTANTI NECESSARIE ALLO SVOLGIMENTO 
-
 const userNumber = Math.floor((Math.random() * 6) + 1); // NUMERO UTENTE
 const computerNumber = Math.floor((Math.random() * 6) + 1); // NUMERO COMPUTER 
 
-// COSTANTE PER RECUPERARE L'ELEMENTO HTML SU CUI SCRIVERE
+// COSTANTI PER RECUPERARE GLI ELEMENTI SU CUI SCRIVERE
 
 const userBox = document.getElementById("user__number");
 const machineBox = document.getElementById("machine__number");
@@ -27,3 +26,29 @@ if (userNumber > computerNumber) {
 
     results.innerHTML = "Peccato, il computer ha vinto ancora..."
 };
+
+
+
+
+// METODO ARRAY
+
+// const userBox = document.getElementById("user__number");
+// const machineBox = document.getElementById("machine__number");
+// const results = document.getElementById("results");
+
+// // // SCRITTURA NUMERI CASUALI NEGLI APPOSITI BOX
+
+// const randomNumber = [1, 2, 3, 4, 5, 6];
+
+// const randomIndex = Math.floor(Math.random() * 6);
+
+// const dice_result = randomNumber[randomIndex];
+
+
+// userBox.innerHTML = userNumber;
+// machineBox.innerHTML = computerNumber;
+
+// for (let i = 0; i < userNumber; i++) {
+
+
+// }
